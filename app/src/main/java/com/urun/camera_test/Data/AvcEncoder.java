fb59a85509b551f418d2504461c23f2030cc7774
ahmeturun <ahmet.urun@outlook.com>
2016-12-23 22:09:05 +0300

    By far the best version with mediacodec but the yv12 color format conversion makes the blue-red inversion.-mediacodec v2.1-

--- app/src/main/java/com/urun/camera_test/Data/AvcEncoder.java
+++ app/src/main/java/com/urun/camera_test/Data/AvcEncoder.java
@@ -1,7 +1,16 @@
+/*
+There is no timestamp information in the raw H.264 elementary stream.
+*You need to pass the timestamp through the decoder to MediaMuxer or
+* whatever you're using to create your final output. If you don't, the
+* player will just pick a rate, or possibly play the frames as fast as it can.
+* answer from: http://stackoverflow.com/questions/19915962/android-mediacodec-video-wrong-color-and-playing-too-fast
+*/
+
 package com.urun.camera_test.Data;
 
 import android.media.MediaCodec;
 import android.media.MediaCodecInfo;
+import android.media.MediaCodecList;
 import android.media.MediaFormat;
 import android.os.Environment;
 import android.util.Log;
@@ -19,8 +28,14 @@ class AvcEncoder {
 
     private MediaCodec mediaCodec;
     private BufferedOutputStream outputStream;
+    private static final String MIME_TYPE = "video/avc";    // H.264 Advanced Video Coding
 
     AvcEncoder() {
+        /*Retrieving codec/codecs on device*/
+        MediaCodecInfo codecInfo = selectCodec(MIME_TYPE);
+        Log.d("MediacodecInfo_retr: ", "found codec: " + codecInfo.getName());
+        int colorFormat = selectColorFormat(codecInfo, MIME_TYPE);
+        Log.d("ColorFormat", "found colorFormat: " + colorFormat);
         File f = new File(Environment.getExternalStorageDirectory(), "/video_encoded.264");
         try {
             outputStream = new BufferedOutputStream(new FileOutputStream(f));
@@ -29,15 +44,16 @@ class AvcEncoder {
             e.printStackTrace();
         }
 
+        /* Create a MediaCodec for the desired codec, then configure it as an encoder with our desired properties.*/
         try {
-            mediaCodec = MediaCodec.createEncoderByType("video/avc");
+            mediaCodec = MediaCodec.createByCodecName(codecInfo.getName());
         } catch (IOException e) {
             e.printStackTrace();
         }
-        MediaFormat mediaFormat = MediaFormat.createVideoFormat("video/avc", 640, 960);
-        mediaFormat.setInteger(MediaFormat.KEY_BIT_RATE, 8000000);
-        mediaFormat.setInteger(MediaFormat.KEY_FRAME_RATE, 30);
-        mediaFormat.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420Planar);
+        MediaFormat mediaFormat = MediaFormat.createVideoFormat(MIME_TYPE, 640, 960);
+        mediaFormat.setInteger(MediaFormat.KEY_BIT_RATE, 2000000);
+        mediaFormat.setInteger(MediaFormat.KEY_FRAME_RATE, 25);
+        mediaFormat.setInteger(MediaFormat.KEY_COLOR_FORMAT, 19);
         mediaFormat.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 5);
         mediaCodec.configure(mediaFormat, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
         mediaCodec.start();
@@ -54,7 +70,6 @@ class AvcEncoder {
         }
     }
 
-    // called from Camera.setPreviewCallbackWithBuffer(...) in other class
     void offerEncoder(byte[] input) {
         try {
             ByteBuffer[] inputBuffers = mediaCodec.getInputBuffers();
@@ -85,5 +100,40 @@ class AvcEncoder {
             t.printStackTrace();
         }
 
+    }
+
+    /*Below two functions has taken from: https://android.googlesource.com/platform/cts/+/jb-mr2-release/tests/tests/media/src/android/media/cts/EncodeDecodeTest.java*/
+    /**
+     * Returns the first codec capable of encoding the specified MIME type, or null if no
+     * match was found.
+     */
+    private static MediaCodecInfo selectCodec(String mimeType) {
+        int numCodecs = MediaCodecList.getCodecCount();
+        for (int i = 0; i < numCodecs; i++) {
+            MediaCodecInfo codecInfo = MediaCodecList.getCodecInfoAt(i);
+            if (!codecInfo.isEncoder()) {
+                continue;
+            }
+            String[] types = codecInfo.getSupportedTypes();
+            for (int j = 0; j < types.length; j++) {
+                if (types[j].equalsIgnoreCase(mimeType)) {
+                    return codecInfo;
+                }
+            }
+        }
+        return null;
+    }
+    /**
+     * Returns a color format that is supported by the codec and by this test code.  If no
+     * match is found, this throws a test failure -- the set of formats known to the test
+     * should be expanded for new platforms.
+     */
+    private static int selectColorFormat(MediaCodecInfo codecInfo, String mimeType) {
+        MediaCodecInfo.CodecCapabilities capabilities = codecInfo.getCapabilitiesForType(mimeType);
+        for (int i = 0; i < capabilities.colorFormats.length; i++) {
+            int colorFormat = capabilities.colorFormats[i];
+            return colorFormat;
+        }
+        return 0;   // not reached
     }
 }
--- app/src/main/java/com/urun/camera_test/Data/MainActivity.java
+++ app/src/main/java/com/urun/camera_test/Data/MainActivity.java
@@ -156,7 +156,8 @@ public class MainActivity extends Activity{
                             canvas.drawBitmap(bitmapBack, 0, 0, paint);
                             canvas.drawBitmap(bitmapFront, 0, bitmapBack.getHeight(), paint);
 
-                            byte[] byteArray = getNV21(bitmapResult.getWidth(), bitmapResult.getHeight(), bitmapResult);//the byte array version of merged pictures
+                            byte[] byteArray = getYV12(bitmapResult.getWidth(), bitmapResult.getHeight(), bitmapResult);//the byte array version of merged pictures
+
                             mAvEncoder.offerEncoder(byteArray);
                             combinedFrameNumber++;
                             Log.e("EncodeSuccesful_Frame: ", "" + combinedFrameNumber);
@@ -171,6 +172,7 @@ public class MainActivity extends Activity{
     }
 
     //below two functions used for converting bitmap file to NV21 taken from: http://stackoverflow.com/questions/5960247/convert-bitmap-array-to-yuv-ycbcr-nv21
+    //////////////////////////////////////////////////////////////////////////7
     byte [] getNV21(int inputWidth, int inputHeight, Bitmap scaled) {
         synchronized (key) {
 
@@ -223,6 +225,61 @@ public class MainActivity extends Activity{
             }
         }
     }
+    //////////////////////////////////////////////////////////////////
+
+    //Below two functions also used in taking yuv colors from bitmap but this time getting the color yv12 format
+    // https://gist.github.com/wobbals/5725412
+
+    private byte [] getYV12(int inputWidth, int inputHeight, Bitmap scaled) {
+
+        int [] argb = new int[inputWidth * inputHeight];
+
+        scaled.getPixels(argb, 0, inputWidth, 0, 0, inputWidth, inputHeight);
+
+        byte [] yuv = new byte[inputWidth*inputHeight*3/2];
+        encodeYV12(yuv, argb, inputWidth, inputHeight);
+
+        scaled.recycle();
+
+        return yuv;
+    }
+
+    private void encodeYV12(byte[] yuv420sp, int[] argb, int width, int height) {
+        final int frameSize = width * height;
+
+        int yIndex = 0;
+        int uIndex = frameSize;
+        int vIndex = frameSize + (frameSize / 4);
+
+        int a, R, G, B, Y, U, V;
+        int index = 0;
+        for (int j = 0; j < height; j++) {
+            for (int i = 0; i < width; i++) {
+
+                a = (argb[index] & 0xff000000) >> 24; // a is not used obviously
+                R = (argb[index] & 0xff0000) >> 16;
+                G = (argb[index] & 0xff00) >> 8;
+                B = (argb[index] & 0xff) >> 0;
+
+                // well known RGB to YUV algorithm
+                Y = ( (  66 * R + 129 * G +  25 * B + 128) >> 8) +  16;
+                U = ( ( -38 * R -  74 * G + 112 * B + 128) >> 8) + 128;
+                V = ( ( 112 * R -  94 * G -  18 * B + 128) >> 8) + 128;
+
+                // YV12 has a plane of Y and two chroma plans (U, V) planes each sampled by a factor of 2
+                //    meaning for every 4 Y pixels there are 1 V and 1 U.  Note the sampling is every other
+                //    pixel AND every other scanline.
+                yuv420sp[yIndex++] = (byte) ((Y < 0) ? 0 : ((Y > 255) ? 255 : Y));
+                if (j % 2 == 0 && index % 2 == 0) {
+                    yuv420sp[uIndex++] = (byte)((V<0) ? 0 : ((V > 255) ? 255 : V));
+                    yuv420sp[vIndex++] = (byte)((U<0) ? 0 : ((U > 255) ? 255 : U));
+                }
+
+                index ++;
+            }
+        }
+    }
+
 
     public static byte[] YV12toYUV420Planar(byte[] input, byte[] output, int width, int height) {
     /*
@@ -448,6 +505,16 @@ public class MainActivity extends Activity{
                 dstData[dstOff + 2] = rgb & 0xff;
             }
         }
+    }
+    public byte[] swapYV12toI420(byte[] yv12bytes, int width, int height) {
+        byte[] i420bytes = new byte[yv12bytes.length];
+        for (int i = 0; i < width*height; i++)
+            i420bytes[i] = yv12bytes[i];
+        for (int i = width*height; i < width*height + (width/2*height/2); i++)
+            i420bytes[i] = yv12bytes[i + (width/2*height/2)];
+        for (int i = width*height + (width/2*height/2); i < width*height + 2*(width/2*height/2); i++)
+            i420bytes[i] = yv12bytes[i - (width/2*height/2)];
+        return i420bytes;
     }
 
     @Override
